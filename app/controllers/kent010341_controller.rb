6c4072077f4fa9792334adceee910b7199e7bb6b
kent010341 <kent010341@gmail.com>
2018-07-11 13:20:49 +0800

    keyword reply

--- app/controllers/kent010341_controller.rb
+++ app/controllers/kent010341_controller.rb
@@ -3,12 +3,42 @@ class Kent010341Controller < ApplicationController
 	protect_from_forgery with: :null_session
 #=======================================================================================================================================
 # line webhook
+
+# config.channel_secret = '120203750409774d33145a57ceb4c6e0'
+# config.channel_token = 'kFlOjWJnFvLcZCtPI4ZgZYsrxrHrgsK9bMPjqxEVtP2bJmdIX3OSgstERol/Ze8iKczr1/9qWIhyl6HDkIi1bcEJjJpVov7izgLyFmGKMRnQ6qIcJYhdFo4XxBFIdIY3ZYUzVy9MoR976fS20dZL0wdB04t89/1O/w1cDnyilFU='
+
 	def webhook
-		# Line Bot API 物件初始化
-		client = Line::Bot::Client.new { |config|
-			config.channel_secret = '120203750409774d33145a57ceb4c6e0'
-			config.channel_token = 'kFlOjWJnFvLcZCtPI4ZgZYsrxrHrgsK9bMPjqxEVtP2bJmdIX3OSgstERol/Ze8iKczr1/9qWIhyl6HDkIi1bcEJjJpVov7izgLyFmGKMRnQ6qIcJYhdFo4XxBFIdIY3ZYUzVy9MoR976fS20dZL0wdB04t89/1O/w1cDnyilFU='
+		# 設定回覆文字
+		reply_text = keyword_reply(received_text)
+
+		# 傳送訊息到 line
+		response = reply_to_line(reply_text)
+
+		# 回應 200
+		head :ok
+	end
+
+	# 取得對方說的話
+	def received_text
+		message = params['events'][0]['message']
+		message['text'] unless message.nil?
+	end
+
+	# 關鍵字回覆
+	def keyword_reply(received_text)
+		# 學習紀錄表
+		keyword_mapping = {
+			'QQ' => '幫Q',
+			'安' => '安安'
 		}
+
+		# 查表
+		keyword_mapping[received_text]
+	end
+
+		# 傳送訊息到 line
+	def reply_to_line(reply_text)
+		return nil if reply_text.nil?
 
 		# 取得 reply token
 		reply_token = params['events'][0]['replyToken']
@@ -16,15 +46,20 @@ class Kent010341Controller < ApplicationController
 		# 設定回覆訊息
 		message = {
 			type: 'text',
-			text: '好哦～好哦～'
-		}
+			text: reply_text
+		} 
 
 		# 傳送訊息
-		response = client.reply_message(reply_token, message)
-
-		# 回應 200
-		head :ok
+		line.reply_message(reply_token, message)
 	end
+
+	# Line Bot API 物件初始化
+	def line
+		@line ||= Line::Bot::Client.new { |config|
+			config.channel_secret = '120203750409774d33145a57ceb4c6e0'
+			config.channel_token = 'kFlOjWJnFvLcZCtPI4ZgZYsrxrHrgsK9bMPjqxEVtP2bJmdIX3OSgstERol/Ze8iKczr1/9qWIhyl6HDkIi1bcEJjJpVov7izgLyFmGKMRnQ6qIcJYhdFo4XxBFIdIY3ZYUzVy9MoR976fS20dZL0wdB04t89/1O/w1cDnyilFU='
+		}
+  end
 
 #=======================================================================================================================================
 # day1~16
